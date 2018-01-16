c4907e954003860044aa4089b92dc493df3d6346
Katherine Donnally <kbd3ax@virginia.edu>
2018-01-15 22:13:33 -0500

    Basic bg styles + auto-fill grid w/ max # square divs
    -NB: if actually load rn, document looks very silly --> can turn off
         border on 'dot' divs to remove red grid.
    
    -Also, the dots are squares for now, for which I am sorry, but we are
     taking care of the (decorative) CSS in the next commit.
    
    -Explanations:
    1.) index.html
      a. Edit font link: nix Open Sans, add Righteous.
      b. Add comments noting qualities/writing out names of various Google
         fonts. (From picking title font, but leaving b/c still useful
         given prototype nature.)
      c. Add div to hold 'dots', w/ 1 starter dot inside.
    
    2.) style.css/style.scss
      a. Center title text on page; assign font styles.
      b. Set a background gradient.
      c. Make 'dot container' a grid with...
        i. as many columns as it can fit, provided they're all the same
           width *and* are all greater than or equal to 60px.
       ii. an indeterminate number of rows. (Enter JS.)
      d. Make each grid item's width and height 100% of the available space.
         (Grid items = 'dots'.)
    
    3.) main.js
      a. Split grid into squares: set each row of the dot container equal to
         the calculated width of each column in the grid. ('Dot' div is now
         square, since height/width are 100% of grid item space.)
      b. Calculate the number of rows and columns there will be, based on
         the column width and height/width of container (which is entire
         viewport).
      c. Fill grid using 'for' loop that appends a clone of the original
         'dot' to the dot container X times, where X = rows * columns.

--- theme-testing/main.js
+++ theme-testing/main.js
@@ -1,3 +1,19 @@
 (function() {
-	// do stuff
+	// set dot height = width
+	var dotsWrapper = document.querySelector(".home-dots-wrapper");
+	var dot = document.querySelector(".home-dots__dot");
+	var width = dot.getBoundingClientRect().width;
+	dot.style.height = width + "px";
+	var dotUpdate = document.querySelector(".home-dots__dot");
+	dotsWrapper.setAttribute("style", `grid-auto-rows:${width}px`);
+	// repeat this on resize
+
+	// calculate # of divs to make (grid matrix dimensions)
+	var cols = Math.floor(dotsWrapper.offsetWidth / width);
+	var rows = Math.floor(dotsWrapper.offsetHeight / width);
+	var numDots = cols * rows;
+
+	for (var i=0; i<numDots; i++) {
+		dotsWrapper.appendChild(dotUpdate.cloneNode());
+	}
 })();
