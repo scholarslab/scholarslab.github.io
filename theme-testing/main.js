54e198bf48385a88846f46a948410c915d1de276
Katherine Donnally <kbd3ax@virginia.edu>
2018-01-15 22:44:14 -0500

    Fix minor math issues/redundancies in dot grid

--- theme-testing/main.js
+++ theme-testing/main.js
@@ -3,17 +3,15 @@
 	var dotsWrapper = document.querySelector(".home-dots-wrapper");
 	var dot = document.querySelector(".home-dots__dot");
 	var width = dot.getBoundingClientRect().width;
-	dot.style.height = width + "px";
-	var dotUpdate = document.querySelector(".home-dots__dot");
 	dotsWrapper.setAttribute("style", `grid-auto-rows:${width}px`);
 	// repeat this on resize
 
 	// calculate # of divs to make (grid matrix dimensions)
 	var cols = Math.floor(dotsWrapper.offsetWidth / width);
-	var rows = Math.floor(dotsWrapper.offsetHeight / width);
+	var rows = Math.floor(dotsWrapper.offsetHeight / width) + 1;
 	var numDots = cols * rows;
 
 	for (var i=0; i<numDots; i++) {
-		dotsWrapper.appendChild(dotUpdate.cloneNode());
+		dotsWrapper.appendChild(dot.cloneNode());
 	}
 })();
