c06b37883b457dc45ae99192f622462a67042c8d
Katherine Donnally <kbd3ax@virginia.edu>
2018-01-22 17:29:07 -0500

    Move main.js to a JS folder; update HTML <script> link w/ it

---
+++ theme-testing/js/main.js
@@ -0,0 +1,31 @@
+(function() { 
+
+	
+	// Section 1
+	// Component: Homepage
+
+	(function() {
+		// set dot height = width
+		var dotsWrapper = document.querySelector(".home-dots-wrapper");
+		var dot = document.querySelector(".home-dots__dot");
+		var width = dot.getBoundingClientRect().width;
+		dotsWrapper.setAttribute("style", `grid-auto-rows:${width}px`);
+		// repeat this on resize ** NEED TO DO! **
+
+		// calculate # of divs to make (grid matrix dimensions)
+		var cols = Math.floor(dotsWrapper.offsetWidth / width);
+		var rows = Math.floor(dotsWrapper.offsetHeight / width) + 1;
+		var numDots = cols * rows;
+
+		for (var i=0; i<numDots; i++) {
+			dotsWrapper.appendChild(dot.cloneNode(true));
+		}
+	})();
+
+	// Section 2
+	// Component: Top nav
+
+	(function() {
+		// do stuff
+	})();
+})();
