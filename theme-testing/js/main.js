124f300ca133ce60dffb0eaab22232ca31ea3919
Katherine Donnally <kbd3ax@virginia.edu>
2018-01-23 15:37:52 -0500

    Create function to change topnav appearance on scroll

--- theme-testing/js/main.js
+++ theme-testing/js/main.js
@@ -21,6 +21,29 @@
 
 	// Section 2: Top nav
 	(function() {
-		// do stuff
+		var topNav = document.querySelector(".include--topnav"); // outer <nav> element
+		var navBackground = document.querySelector(".topnav__background");
+
+		function changeNavHeight() {
+			// if offsetTop of document is >0 (may change), add class that makes topnav height smaller or 0
+			// this part will need nuance ^^^
+			// if scroll up by 50px or more - so if change in scrollY is less than -50px? - remove class
+			// when to trigger:
+
+			// oh or just do the background opacity thing???  
+			console.log(window.scrollY);
+			var lastPosition = window.scrollY;
+		//	if (lastPosition > window.innerHeight) {
+		//		topNav.classList.add("topnav-scroll--hide");
+		//	}
+			if (lastPosition > topNav.offsetHeight) {
+				navBackground.classList.add("topnav-scroll--down");
+			}
+			else if (lastPosition <= topNav.offsetHeight) {
+				navBackground.classList.remove("topnav-scroll--down");
+			}
+		}
+
+		window.addEventListener("scroll", changeNavHeight);
 	})();
 })();
