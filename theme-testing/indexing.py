e437dc6098027fb39e6541f4b7f045b6fb3972c2
Katherine Donnally <kbd3ax@virginia.edu>
2018-01-24 16:30:53 -0500

    Make Python write semantic class HTML for home
    -Also have built up enough experimenting that everything's modulated
     into functions now

--- theme-testing/indexing.py
+++ theme-testing/indexing.py
@@ -7,7 +7,9 @@ newnums = ["0." + num for num in nums]
 #for num in newnums:
 #	print(num)
 
-# for homepage grid:
+# for homepage test grid (evening of 1/23/18)
+
+
 
 s = "small"
 m = "medium"
@@ -19,4 +21,57 @@ nums = [str(n) for n in range(1,23)]
 
 for size, letter, num in zip(sizes, sizeinitials, nums):
 	div = '''<div id="{1}{2}" class="home-item--{0}">{2}, {0}</div>'''.format(size, letter, num)
-	print(div)
+#	print(div)
+
+# for homepage round 2 (1/24/18):
+
+# pasted + lightly edited from 'Design Notes' spreadsheet
+contents = '''Hero text
+contact link 1
+FAQ link
+Charter link
+office hours
+floor diagram
+GIS corner
+Makerspace corner
+Grads
+Events
+Learn
+Blog
+Staffer projects
+Contact link 2
+listserv prompt
+People
+Origin stories'''
+
+def baseClasses(contents):
+	classbasis = contents.lower().replace(' ', '-').splitlines()
+	return classbasis
+
+def makeHomeClasses(classbasis):
+	home_classes = ["home__" + c for c in classbasis]
+	# shortened further if 17+ chars -> these were originally global vars
+	class_lengths = [len(c) for c in classbasis]
+
+	return home_classes
+
+def makeHomeDivs(home_classes, classbasis):
+	divs = []
+	for h, c in zip(home_classes, classbasis):
+		div = '''<div class="{0}">{1}</div>'''.format(h, c)
+		divs.append(div)
+	return divs
+
+# split into list on newlines -> class name base strings
+classbasis = baseClasses(contents)
+
+# add "home__" prefix per BEM specification
+home_classes = makeHomeClasses(classbasis)
+# print(home_classes)
+
+# make 1 div per class created
+home_divs = makeHomeDivs(home_classes, classbasis)
+
+# print for HTML
+''' for h in home_divs:
+	print(h) '''
