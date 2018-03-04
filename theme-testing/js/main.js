95f439b1140c75a5bab05f651290d1f724faffd3
Katherine Donnally <kbd3ax@virginia.edu>
2018-03-04 14:59:24 -0500

    Add JS to show back/up nav if scroll up

--- theme-testing/js/main.js
+++ theme-testing/js/main.js
@@ -2,21 +2,25 @@
 	// Section 2.1: Top nav 2, electric boogaloo
 	(function() {
 		var wrapper = document.querySelector(".nav-wrapper");
+		wrapper.setAttribute("data-scroll-y", window.scrollY);
 
-		function changeNavHeight() {
-			console.log(window.scrollY);
-			var lastPosition = window.scrollY;
-		//	if (lastPosition > window.innerHeight) {
-		//		topNav.classList.add("topnav-scroll--hide");
-		//	}
-			if (lastPosition > topNav.offsetHeight) {
-				navBackground.classList.add("topnav-scroll--down");
+		function showHideNav() {
+			console.log(window.scrollY.toFixed(2));
+			console.log(wrapper.getAttribute("data-scroll-y"));
+			var lastPosition = parseInt(wrapper.getAttribute("data-scroll-y"));
+
+			if (window.scrollY < lastPosition && window.scrollY >= window.innerHeight / 2) {
+			 	console.log("UP");
+				wrapper.classList.add("nav-wrapper--scroll-up");
 			}
-			else if (lastPosition <= topNav.offsetHeight) {
-				navBackground.classList.remove("topnav-scroll--down");
+			else if (window.scrollY > lastPosition || window.scrollY.toFixed(1) <= 10) {
+				console.log("DOWN");
+				wrapper.classList.remove("nav-wrapper--scroll-up");
 			}
+			
+			wrapper.setAttribute("data-scroll-y", window.scrollY);
 		}
 
-		window.addEventListener("scroll", changeNavHeight);
+		window.addEventListener("scroll", showHideNav, false);
 	})();
 })();
