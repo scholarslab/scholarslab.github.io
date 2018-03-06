c33084b928ea09ab9bde073dfdb9dad3cda9c45d
Katherine Donnally <kbd3ax@virginia.edu>
2018-03-06 18:22:01 -0500

    Create Python module that takes HTML -> returns CSS
    -Why: created a bunch of new class names in HTML file and didn't want
          to name them all individually in _makerspace.scss
    
    -What: collection of Python functions that you can plug in any HTML
           file into & it'll print formatted CSS selectors for all classes
           detected. (Definitely some magic numbers/chars rn, any reuse
           needs slight tweaking to input values!)

---
+++ theme-testing/file-text-search.py
@@ -0,0 +1,57 @@
+# file-text-search.py
+# this module returns all class declarations in a given HTML file
+
+# using regex to search and grab
+def regexTextSearch(file, pattern):
+	import re
+
+	with open(file, 'rt') as myfile:
+		matches = re.findall(pattern, myfile.read())
+		return matches
+
+# take each 'class="<val>"' & return <val>
+def stripClassStrings(matches):
+	quoted_names = [m.replace('class=', '') for m in matches]
+	class_names = [q.replace('"', '') for q in quoted_names]
+	return class_names
+
+''' ASIDE: '''
+# list of all section-level classes
+def sectionLevelClasses(class_names):
+	css = []
+	for i, c in enumerate(class_names):
+		if c.startswith("makerspace"):
+			css.append(c)
+	return css
+
+def createGridAreas(sections):
+	areas = [s.split('__')[1] for s in sections if '__' in s]
+	return areas
+''' /END ASIDE '''
+
+# take class names -> CSS selectors
+def classesToCSS(class_names):
+	css = []
+	for i, c in enumerate(class_names):
+		if '__' in c:
+			selector = '''.{0} {{\n\n}}\n'''.format(c)
+			css.append(selector)
+	return css
+
+# match string s where s == 'class="<val>"'
+# NB: throws error if quoted, e.g. "r'<pattern>'"
+pattern = r'class=".*?"'
+
+# list of all 'class="<val>"' in file
+matches = regexTextSearch('pages/makerspace.html', pattern)
+
+# list of all val for 'class="<val>"' in matches
+class_names = stripClassStrings(matches)
+
+# section levels (use for grid-areas)
+sections = sectionLevelClasses(class_names)
+
+
+css = classesToCSS(class_names)
+for c in css:
+	print(c)
