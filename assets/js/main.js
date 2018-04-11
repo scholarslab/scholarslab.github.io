5f98b6efa2182fe9978c02c6539175fdb5e3b547
Katherine Donnally <kbd3ax@virginia.edu>
2018-04-11 14:13:27 -0400

    Equalize gallery item height/width on load/resize

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,19 @@
 (function() {
+	// Research gallery:
+	(function() {
+		var items = document.querySelectorAll(".research__item");
 
+		document.addEventListener("DOMContentLoaded", setItemHeight)
+
+		window.addEventListener("resize", setItemHeight);
+
+		// equalize width & height
+		function setItemHeight() {
+			var width = items[0].offsetWidth;
+			for (let item of items) {
+				var newHeight = width.toString() + "px";
+				item.style.height = newHeight;
+			}
+		}
+	})();
 })();
