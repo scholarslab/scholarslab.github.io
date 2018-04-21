50793865ac11c5f8ec12b011a2ef88d48906699c
Katherine Donnally <kbd3ax@virginia.edu>
2018-04-21 18:29:42 -0400

    Add 'if' clause so JS only executes on pages where relevant
    - Only executes if element w/ given class name is on the page
    - Attaching to all future fxns as well

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,26 @@
 (function() {
 	// Research gallery:
 	(function() {
-		var items = document.querySelectorAll(".research__item");
-		var galleries = document.querySelectorAll(".research-items-wrapper");
+		if (document.querySelector(".research-main-content")) {
+			var items = document.querySelectorAll(".research__item");
+			var galleries = document.querySelectorAll(".research-items-wrapper");
 
-		document.addEventListener("DOMContentLoaded", setItemHeight)
+			document.addEventListener("DOMContentLoaded", setItemHeight)
 
-		window.addEventListener("resize", setItemHeight);
+			window.addEventListener("resize", setItemHeight);
 
-		// equalize width & height
-		function setItemHeight() {
-			var width = items[0].offsetWidth;
-			for (let item of items) {
-				var newHeight = width.toString() + "px";
-				item.style.height = newHeight;
-			}
+			// equalize width & height
+			function setItemHeight() {
+				var width = items[0].offsetWidth;
+				for (let item of items) {
+					var newHeight = width.toString() + "px";
+					item.style.height = newHeight;
+				}
 
-			var rowHeight = (width / 2).toString() + "px";
-			for (let gallery of galleries) {
-				gallery.style.gridAutoRows = "minmax(" + rowHeight + ", auto)";
+				var rowHeight = (width / 2).toString() + "px";
+				for (let gallery of galleries) {
+					gallery.style.gridAutoRows = "minmax(" + rowHeight + ", auto)";
+				}
 			}
 		}
 	})();
