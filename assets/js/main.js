573e02eed5ef7d3fc1d5f836e085f4de43167dc3
Katherine Donnally <kbd3ax@virginia.edu>
2018-04-21 18:31:18 -0400

    Add function to equalize width/height of recent posts
    - Sets the same way as logic on research page
      (as squares based on width of viewport)

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,4 +24,36 @@
 			}
 		}
 	})();
+
+	// Recent posts:
+	(function() {
+		if (document.querySelector(".person-wrapper")) {
+			var postList = document.querySelector(".person-posts__list");
+			var posts = postList.children;
+
+			document.addEventListener("DOMContentLoaded", setItemHeight)
+
+			window.addEventListener("resize", setItemHeight);
+
+			// equalize width & height
+			function setItemHeight() {
+
+				var width = posts[0].offsetWidth;
+				var n = 1;
+				for (let post of posts) {
+					post.style.gridColumn = n.toString();
+				//	let width = post.offsetWidth / posts.length;
+				//	var newHeight = width.toString() + "px";
+					post.style.height = width.toString() + "px";
+					// increment
+					n = n+1;
+				}
+
+		//		var rowHeight = (width / 2).toString() + "px";
+		//		for (let gallery of galleries) {
+		//			gallery.style.gridAutoRows = "minmax(" + rowHeight + ", auto)";
+		//		}
+			}	
+		}
+	})();
 })();
