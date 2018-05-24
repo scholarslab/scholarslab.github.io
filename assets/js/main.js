ed22ba82880497ba0dbe4f94b1624b587f1b9cd4
Katherine Donnally <kbd3ax@virginia.edu>
2018-05-24 18:10:25 -0400

    Style single-line paragraphs of bold text as section titles
    - People consistently label sections like this:
    
        **Section of Thing**
    
        More text lorem ipsum dolor...
    
    - That renders as "<p><strong>Section of Thing</strong></p>", but it
      would be more appropriate if it were like, an <h3> or something
    
    - So, using JS to apply a CSS class that mimics a section title
      by selecting all elements whose *only* internal content is a set
      of <strong> tags (w/ text b/t)

--- assets/js/main.js
+++ assets/js/main.js
@@ -164,5 +164,12 @@
 			first.classList.add("post-content__letter--default");
 			elems[1].classList.add("post-content__letter--styled");
 		}
+		// test for elems that are a) bold and b) have only one child
+		// Markdown would be: **Newlines surrounding this statement**
+		for (let elem of elems) {
+			if (elem.querySelector("strong") && elem.children.length === 1) {
+				elem.classList.add("post__section-title");
+			}
+		}
 	}
 })();
