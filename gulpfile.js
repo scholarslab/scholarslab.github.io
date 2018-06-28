cbccdf513de84f8e7065395f5f889e2eff6e92a4
Katherine Donnally <kbd3ax@virginia.edu>
2018-06-27 21:59:31 -0400

    Add function to format JS + tasks to watch/write JS to main file

--- gulpfile.js
+++ gulpfile.js
@@ -6,3 +6,22 @@ var gulp   = require('gulp'),
 	concat = require('gulp-concat'),
 	uglify = require('gulp-uglify-es'),
 	insert = require('gulp-insert');
+
+var js_format = () => {
+	return gulp.src('./assets/js/_partials/*.js')
+		.pipe(concat('main.js'))
+		.pipe(uglify({ keep_fnames: true }))
+		.pipe(insert.wrap('(function() {', '\n})();'))
+		.pipe(gulp.dest('./assets/js'));
+}
+
+gulp.task('js_watch', () => {
+	return gulp.watch('./assets/js/_partials/*.js', js_format);
+});
+
+gulp.task('js_clean', () => {
+	return gulp.src('./assets/js/main.js', { allowEmpty: true })
+		.pipe(clean());
+});
+
+gulp.task('js', gulp.series('js_clean', 'js_watch'));
