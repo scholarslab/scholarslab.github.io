930d378af435225e2467738f8077ec589b10038a
Jeremy Boggs <jeremyboggs@gmail.com>
2018-11-20 11:02:40 -0500

    Updates search script.
    - Adds work Lunr search to support query strings, avoid data redundancy;
    - Adds checks for entry metadata, only display things like author,
    categories, and date if those values actually exist;
    
    Fixes #541, in some ways I suppose.

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,35 +1,60 @@
-jQuery(function() {
+var promises = [],
+    searchQuery = window.location.search.substring(1);
 
-  $.getJSON('/search_index.json', (data, err) => {
-    window.idx = data;
-  });
+var jsonFiles = ['/search_index.json','/corpus.json'];
 
-  $.getJSON('/corpus.json', (data, err) => {
-      window.documents = [];
-      Object.entries(data).forEach((key, value)=> {
-          var doc = {
-              'title' : key[1].title,
-              'content': key[1].content,
-              'author': key[1].author,
-              'categories': key[1].categories,
-              'url': key[1].url,
-              'id': key[1].id,
-              'date': key[1].date,
-              'layout': key[1].layout,
-          };
-          window.documents.push(doc);
-      });
-  });
-  // Event when the form is submitted
-  $("#site_search").submit((event) => {
-      event.preventDefault();
-      var query = $("#search_box").val(); // Get the value for the text field
-      window.index = lunr.Index.load(window.idx);
-      var results = window.index.search(query); // Get lunr to perform a search
-      display_search_results(results); // Hand the results off to be displayed
-  });
+// Loop over JSON files, make a XML Http Request, and push result to the `promises` array.
+jsonFiles.forEach(function(f) {
+    promises.push(xhrRequest(f));
+});
 
-  var buildSearchResult = (doc) => {
+// If we've made all our promises.
+Promise.all(promises).then(function(values) {
+    if (searchQuery) {
+        var idx = lunr.Index.load(JSON.parse(values[0]));
+        var docs = JSON.parse(values[1]);
+        var lunrQueryString = buildLunrQueryString(searchQuery);
+        console.log(lunrQueryString);
+        var results = idx.search(lunrQueryString);
+        displaySearchResults(results, docs, 'search_results');
+    }
+}, function(err) {
+    console.log(err)
+});
+
+
+
+// --- Helper Functions ---
+
+// Displays search results
+function displaySearchResults(results, docs, search_container) {
+    var search_results= document.getElementById(search_container);
+    
+    if (results.length) {
+        console.log(results);
+        search_results.innerHTML = "";
+        
+        results.forEach(function(result) {
+            var item = docs.filter(doc => doc.id === result.ref);
+            var html = resultSnippet(item[0]);
+
+            Object.keys(result.matchData.metadata).forEach(function(term) {
+                Object.keys(result.matchData.metadata[term]).forEach(function(fieldName) {
+                    var field = html.querySelector('[data-field=' + fieldName + ']'),
+                        positions = result.matchData.metadata[term][fieldName].position;
+                    wrapTerms(field, positions);
+                });
+            });
+
+            search_results.appendChild(html);
+        });
+
+    } else {
+        search_results.innerHTML = "No results found for your query.";
+    }
+}
+
+function resultSnippet(doc) {
     var li = document.createElement('li'),
         article = document.createElement('article'),
         header = document.createElement('header'),
@@ -39,27 +64,38 @@ jQuery(function() {
         p1 = document.createElement('p'),
         h4 = document.createElement('h4')
 
+    publicationDate = '';
+    if (publicationDate = doc.date) {
+        publicationDate = publicationDate.split(' ')[0] + " EST"; // Get YYYY-MM-DD
+        publicationDate = new Date(publicationDate);
+        var options = { year: 'numeric', month: 'long', day: 'numeric' };
+        publicationDate = publicationDate.toLocaleString('en-US', options);
+    }
+
     a.dataset.field = 'title';
     let slug = doc.id.split('-')
-    console.log(slug, doc);
     a.href;
     if (doc.layout == 'post'){
         a.href = '/' + slug[0] + '/' + slug[1] + '/' + slug[2] + '/' + doc.url;
     } else  {
         a.href = '/' + doc.layout + '/' + doc.url;
     }
-    console.log(a.href);
-    //   /:year/: month /: day /: title /
-    // if (doc.categories !== null){
-    //     doc.categories.forEach( (cat)=>{
-    //         a.href += '/'+ cat;
-    //     });
-    // }
-    // a.href += '/' + doc.url;
+ 
     a.textContent = doc.title;
 
-    p1.dataset.field = 'date';
-    p1.textContent = 'author: '+ doc.author + ' / categories: ' + doc.categories + ' / published: ' + moment(doc.date, 'YYYY-MM-DD').format("MMMM Do YYYY");
+    postMetadata = [];
+
+    if (doc.author) {
+        authorName = doc.author.split('-').map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+    .join(' ');
+        postMetadata.push('author: ' + authorName);
+    }
+    if (doc.categories) {
+        postMetadata.push('categories: ' + doc.categories);
+    }
+    if (publicationDate) {
+        postMetadata.push('published: ' + publicationDate);
+    }
 
     h4.dataset.field = 'content';
     h4.textContent = doc.content;
@@ -73,35 +109,71 @@ jQuery(function() {
     header.appendChild(h2);
     h2.appendChild(a);
     section.appendChild(h4);
-    section.appendChild(p1);
+    if (postMetadata) {
+        p1.textContent = postMetadata.join(' / ');
+        section.appendChild(p1);
+    }
 
     return li;
   }
 
-  function display_search_results(results) {
-      var search_results = $("#search_results");
-      if (results.length) {
-          search_results.empty(); // Clear any old results
+// Returns a Promise on successful XMLHttpRequest.
+function xhrRequest(url) {
+    return new Promise(function(resolve, reject) {
+        var request = new XMLHttpRequest();
+        request.open('get',url);
+        request.onload = function() {
+            if (request.status === 200) {
+                resolve(request.response);
+            } else {
+                reject(Error("File failed to load. Error code: " + request.StatusText));
+            }
+        };
+        request.onerror = function() {
+            reject(Error("The request failed."));
+        };
+        request.send();
+    });
+}
 
-          results.forEach(function(result) {
-              var item = window.documents.filter(doc => doc.id === result.ref);
-              var li = buildSearchResult(item[0])// Build a snippet of HTML for this result
-              Object.keys(result.matchData.metadata).forEach(function (term) {
-                  Object.keys(result.matchData.metadata[term]).forEach(function (fieldName) {
-                      var field = li.querySelector('[data-field=' + fieldName + ']'),
-                      positions = result.matchData.metadata[term][fieldName].position
-                      wrapTerms(field, positions);
-                  });
-              });
-              search_results.append(li);
-            });
-        } else {
-            // If there are no results, let the user know.
-            search_results.html('<li>No results found.<br/>Please check spelling, spacing, yada...</li>');
+// Returns URL query string as an array.
+function returnQueryArray(query_string) {
+    var query_array = new Array();
+    var searchVars = query_string.split('&');
+    if (searchVars) {
+        for (var i=0; i<searchVars.length;i++) {
+            var pair = searchVars[i].split("=");
+            query_array[pair[0]] = pair[1];
         }
     }
+    return query_array;
+}
 
-  function wrapTerms(element, matches) {
+// Builds a Lunr query string from an array.
+function buildLunrQueryString(queryString, searchInputName="keywords") {
+    var queryArray = new Array(),
+        LunrQueryString = '',
+        vars = returnQueryArray(queryString);
+    if (vars) {
+        console.log(vars);
+        for (var key in vars) {
+            value = vars[key];
+            if (value != '') {
+                if (key == searchInputName) {
+                    termsArray = value.split(' ');
+                    queryVariable = '+'+termsArray.join(' +');
+                } else {
+                    queryVariable = "+"+key+":"+value;
+                }
+                queryArray.push(queryVariable);
+            }
+        }
+        LunrQueryString = queryArray.join(" ");
+    }
+    return LunrQueryString;
+}
+
+function wrapTerms(element, matches) {
     var nodeFilter = {
       acceptNode: function (node) {
         if (/^[\t\n\r ]*$/.test(node.nodeValue)) {
@@ -144,7 +216,6 @@ jQuery(function() {
         range.setStart(node, rangeStart)
         range.setEnd(node, rangeEnd)
         range.surroundContents(tag)
-        console.log(range)
         index = match[0] + match[1]
 
         // the next node will now actually be the text we just wrapped, so
@@ -156,5 +227,4 @@ jQuery(function() {
         index = nodeEndIndex
       }
     }
-  }
-});
+}
