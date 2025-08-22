8b4ba98c5e16c2ce41103f594a83e7b368ce2a3b
Jeremy Boggs <jeremy@virginia.edu>
2025-08-22 08:56:29 -0400

    Makes navigation more readable.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
 	const nav = document.querySelector("nav");
 	const menuElement = nav.querySelector("ul");
-	const controllerElement = nav.querySelector("button");
+	const controllerElement = document.getElementById("global-navigation-toggle");
 
 	const menu = new TopLinkDisclosureMenu({
-	  menuElement,
-	  submenuItemSelector: ".dropdown",
-	  containerElement: nav,
-	  controllerElement,
-	  optionalKeySupport: true,
-	  hoverType: "on", // "on", "dynamic"
+		menuElement,
+		submenuItemSelector: ".dropdown",
+		containerElement: nav,
+		controllerElement,
+		optionalKeySupport: true,
+		hoverType: "on", // "on", "dynamic"
 	});
-  });
+
+	controllerElement.addEventListener("click", function() {
+		var isExpanded = this.getAttribute('aria-expanded');
+		if (isExpanded == "false") {
+			this.textContent = "☰";
+		} else {
+			this.textContent = "×";
+		}
+	});
+});
