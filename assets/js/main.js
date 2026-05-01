63bdd2dda97a5a068f3c2329ec7e77be2e1d2a1e
winnieepm <71612467+winnieepm@users.noreply.github.com>
2026-05-01 11:00:22 -0400

    rm unecessary code

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,26 +21,17 @@ document.addEventListener("DOMContentLoaded", () => {
 	});
 	
 	// click event for Alumni Placements 
-	document.querySelectorAll(".job").forEach(function(clickedBubble) {
+	 var jobBubbles = document.querySelectorAll(".job"); 
+
+	jobBubbles.forEach(function(clickedBubble) {
 		clickedBubble.addEventListener("click", function() {
 			const isExpanded = clickedBubble.classList.contains("is-expanded");
-			const uvaProgramDiv = document.createElement('div');
-			const uvaProgramClass = uvaProgramDiv.classList.contains("uva-program");
-			const bubbleParent = // clickedBubble.closest('expanded-bubble');
-			
-			document.querySelectorAll(".job").forEach(function(bubble) {
+
+			jobBubbles.forEach(function(bubble) {
 				bubble.classList.remove("is-expanded");
 			});
-
-			if (!isExpanded) clickedBubble.classList.add("is-expanded"); 
 			
-			if (isExpanded && !uvaProgramClass) bubbleParent.appendChild("uvaProgramDiv");
-			uvaProgramDiv.classList.add("uva-program");
-
-			// remove .uva-program from all .job
-			// on click, create div.uva-program right to div.job;
-			// add ul inside div.uva-program and how many ppl each;
-
+			if (!isExpanded) clickedBubble.classList.toggle("is-expanded");
 		});
 	});
 });
